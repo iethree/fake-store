edd1ba93935fe7bc96ac8d959c1772d85d9e4784
Ryan Laurie <iethree@gmail.com>
2026-02-13 13:13:40 -0700

    initial commit

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Checkout() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      fake store home
+      fake store checkout
     </div>
   );
 }
