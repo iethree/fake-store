f75165ca0ca6f3578c6590ac5da8a8ff233fb8d4
Ryan Laurie <iethree@gmail.com>
2026-02-18 15:25:31 -0700

    test removing config

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Checkout() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       fake store account
+      <a href="/checkout">Go to checkout</a>
     </div>
   );
 }
